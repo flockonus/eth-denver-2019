b2bb6eb83f690a34b19f27f5383f5d5b9886ae3f
FabianoPS <fabianosoriani@gmail.com>
2019-02-16 12:02:36 -0700

    plot set get on SC

--- contract/contracts/City.sol
+++ contract/contracts/City.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.25 <0.6.0;
 contract City {
     struct Game {
         // a board size
-        uint8 size;
+        uint8 boardSize;
         // up to how many players the game MUST have
         uint8 partySize;
         // limit of time for all players to join a session
@@ -23,15 +23,23 @@ contract City {
     // all plots start owned by 0x0, and they are Residence, of value 1
     struct Plot {
         address owner;
-        PlotType property;
+        // 0: residential, 1: commercial, 2: industrial
+        uint8 zone;
         // value declared by the player. Special case is val 0, which becomes 1.
         uint32 value;
     }
 
-    enum PlotType { Residential, Commercial, Industrial }
+    // enum PlotType { Residential, Commercial, Industrial } -- garbage enum in solidity
+
+    event GameCreated(uint indexed gameId, uint boardSize, uint partySize, uint joinLimitAt, uint buyIn);
+    event Join(uint indexed gameId, address player);
+    event GameStart(uint256 indexed gameId);
+    event PlotSet(uint indexed gameId, uint round, address owner, uint8 x, uint8 y, uint8 zone);
     
     mapping(uint => Game) public games;
     uint public gameCount;
+
+    int constant INIT_POINTS = 1000;
 
     constructor() public {
         // what?
@@ -40,10 +48,10 @@ contract City {
     function createGame(uint _buyIn) public payable {
         require(_buyIn < 100 ether, "buy in too high");
         require(msg.value == _buyIn, "send balance");
-        uint8 _size = 4;
+        uint8 _boardSize = 4;
         Game memory game = Game({
             // TODO fix those arbitrary values
-            size: _size,
+            boardSize: _boardSize,
             partySize: 2,
             // arbitrary limit
             joinLimitAt: uint64(now + 1 hours),
@@ -55,21 +63,64 @@ contract City {
         games[gameCount] = game;
         games[gameCount].players.push(msg.sender);
         // 1k initial balance!
-        games[gameCount].balances[msg.sender] = 1000;
+        games[gameCount].balances[msg.sender] = INIT_POINTS;
+        emit Join(gameCount, msg.sender);
+        
         // _initBoard(boardId);
+        emit GameCreated(gameCount, game.boardSize, game.partySize, game.joinLimitAt, game.buyIn);
     }
 
-    // function _initBoard(uint boardId) internal {
-    //     games[boardId]
-    // }
+    function joinGame(uint gameId) public payable {
+        Game storage game = games[gameId];
+        require(game.boardSize > 1, "game doesnt exist");
+        require(game.startedAt == 0, "cant join game started");
+        require(game.balances[msg.sender] == 0, "cant join twice");
+        require(game.buyIn == msg.value, "must pay exact buy in");
 
-    function joinGame() public {
-        // check if game hasnt started yet
-        // add player to array
-        // if player is last to join then start the game!
+        game.players.push(msg.sender);
+        // 1k initial balance!
+        game.balances[msg.sender] = INIT_POINTS;
+        emit Join(gameId, msg.sender);
+
+        if (game.players.length == game.partySize) {
+            game.startedAt = uint64(now);
+            emit GameStart(gameId);
+        }
     }
+
+    /// @dev debug so we can set any cell
+    function _setPlot(uint gameId, uint8 x, uint8 y, uint8 zone, address owner, uint32 price) public {
+        Game storage game = games[gameId];
+        require(game.boardSize > 1, "game doesnt exist");
+        require(game.startedAt > 0, "game didnt start");
+        require(price < INIT_POINTS * 10, "price over limit");
+        require(game.balances[owner] > 0, "player is broke");
+        
+        // TODO some validation on the cell
+
+        uint plotId = (y * game.boardSize) + x;
+        game.plots[plotId] = Plot({
+            owner: owner,
+            zone: zone,
+            value: price
+        });
+        
+        uint8 round = 1;
+        
+        emit PlotSet(gameId, round, owner, x, y, zone);
+    }
+
+    function getPlot(uint gameId, uint8 x, uint8 y) public view returns (address, uint8, uint32) {
+        Game storage game = games[gameId];
+        require (game.boardSize > 0, "game doesnt exist");
+        Plot storage plot = game.plots[(y * game.boardSize) + x];
+        return (plot.owner, plot.zone, plot.value);
+    }
+
+    // function _calcRules()
 
     // TODO -- this is the ideal scenario
+    // function amount()...
     // // salt should not repeat among players and should change at least every round
     // function hashMyMove(uint gameId, uint round, uint landId, uint bid, bytes1 property, uint salt) public view returns (bytes32) {
     //     // TODO a simple hash over the params ABI packed
@@ -79,7 +130,7 @@ contract City {
     // }
 
     // a non-authed version of the above!
-    function submitMove2(params...) {
+    // function submitMove2(params...) {
 
-    }
+    // }
 }
--- contract/test/city.js
+++ contract/test/city.js
@@ -1,7 +1,42 @@
 const City = artifacts.require('City');
 
+function ppLogs(tx) {
+  const { logs } = tx;
+  console.log('logs:', logs.map((l) => ({ label: l.event, arg: l.args })));
+}
+
+const ZONES = {
+  RESIDENTIAL: 0,
+  COMMERCIAL: 1,
+  INDUSTRIAL: 2,
+};
+
+function cropAddress(addr) {
+  return '..' + addr.substring(39);
+}
+
+// a plot obj from SC
+function ppPlot(plot) {
+  let zone = 'R';
+  if (plot[1].eq(1)) zone = 'C';
+  if (plot[1].eq(2)) zone = 'I';
+
+  return `${cropAddress(plot[0])} | ${zone} | ${plot[2].toNumber()}`;
+}
+
+async function debugBoard(gameId, city) {
+  for (let x = 0; x < 4; x++) {
+    for (let y = 0; y < 4; y++) {
+      const plot = await city.getPlot(gameId, x, y);
+      console.log(`[${x}, ${y}]`, 'plot', ppPlot(plot));
+    }
+  }
+}
+
 contract('City', (accounts) => {
   let city;
+  const p1 = accounts[0];
+  const p2 = accounts[1];
 
   before(async function() {
     city = await City.new();
@@ -9,11 +44,23 @@ contract('City', (accounts) => {
 
   it('createGame', async function() {
     // create a game and stake
-    await city.createGame(1, { value: 1 });
+    const tx = await city.createGame(1, { value: 1, from: p1 });
+    // ppLogs(tx); // looking good! Join + GameCreated
   });
 
   it('join game & start it', async function() {
-    // create a game and stake
-    await city.createGame(1, { value: 1 });
+    const tx = await city.joinGame(1, { value: 1, from: p2 });
+    // ppLogs(tx); // looking good! Join + GameStart
+  });
+
+  it('set some plots', async function() {
+    //
+    let tx;
+    let price = 10;
+    tx = await city._setPlot(1, 0, 0, ZONES.RESIDENTIAL, p1, price);
+    tx = await city._setPlot(1, 3, 3, ZONES.RESIDENTIAL, p1, price);
+    ppLogs(tx);
+
+    await debugBoard(1, city);
   });
 });
