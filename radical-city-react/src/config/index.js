f19db5ef9b6f828b424e19470f0ada0738e1ba37
Ian Pun <ian.pun@dapperlabs.com>
2019-02-16 16:45:29 -0700

    update

---
+++ radical-city-react/src/config/index.js
@@ -0,0 +1,7 @@
+const Config = {
+  gameContract: '0x5f43059D07a1AF748CA3590AED2f4b3f861D014b',
+  networkId: 4,
+  blockNativeAPIKey: process.env.BLOCK_NATIVE_API_KEY,
+};
+
+export default Config;
