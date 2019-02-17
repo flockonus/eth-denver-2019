890c3eefe321c9ebbeb996c388ff5aa554bf0f6e
FabianoPS <fabianosoriani@gmail.com>
2019-02-17 03:13:02 -0700

    new deployment and ABI

--- radical-city-react/src/config/index.js
+++ radical-city-react/src/config/index.js
@@ -1,5 +1,5 @@
 const Config = {
-  gameContractAddr: '0x04d9c9017e3c531598c9e81cb0b03b560e477f57',
+  gameContractAddr: '0xe502b756a4d471e4d60cecb8033fe7f3629d2900',
   networkId: 4,
   blockNativeAPIKey:
     process.env.BLOCK_NATIVE_API_KEY || '64825f22-6a27-454b-ae2d-c1f2fa8e6fab',
