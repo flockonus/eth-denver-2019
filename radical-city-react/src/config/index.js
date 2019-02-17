ec4b659820499965c865298e7695dc8c4699fd7b
FabianoPS <fabianosoriani@gmail.com>
2019-02-17 12:11:21 -0700

    more test

--- radical-city-react/src/config/index.js
+++ radical-city-react/src/config/index.js
@@ -2,7 +2,8 @@ const Config = {
   // gameContractAddr: '0xe502b756a4d471e4d60cecb8033fe7f3629d2900', // Rinkeby old
   // gameContractAddr: '0xf9416fbe3bbdd5ed0d028d2fc760a92685310443', // rinkeby new
   // gameContractAddr: '0x45269e73Ba7E35c8E364D9a36E496Ac8eb67c166', // SKALE
-  gameContractAddr: '0x459b36Af59BC202C86A5Affa40Fc0238E157dbe1',
+  // gameContractAddr: '0x459b36Af59BC202C86A5Affa40Fc0238E157dbe1',
+  gameContractAddr: '0xb6c7F8ffaDA927C3A7A3d5D880c7D0072Df5f897',
   networkId: 4,
   blockNativeAPIKey:
     process.env.BLOCK_NATIVE_API_KEY || '64825f22-6a27-454b-ae2d-c1f2fa8e6fab',
